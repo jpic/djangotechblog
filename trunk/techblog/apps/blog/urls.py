d852e8268a867b62d7148147bce4507d460451c3
Will McGugan <x@example.com>
2008-11-06 21:01:38 +0000

    cleaning up mistake

--- trunk/techblog/apps/blog/urls.py
+++
@@ -1,8 +0,0 @@
-import views
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-
-    ('^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<slug>.*)/$', views.blog_entry),
-    
-    )
