01d9a81f65ff23f95ebd0837005b6f23c813d437
Will McGugan <x@example.com>
2008-11-16 18:18:38 +0000

    Style changes mostly

--- techblog/apps/blog/markuptags.py
+++ techblog/apps/blog/markuptags.py
@@ -76,9 +76,9 @@ class HTMLTag(postmarkup.TagBase):
     def __init__(self, name, **kwargs):
         postmarkup.TagBase.__init__(self, name, enclosed=True, strip_first_newline=True)
 
-    def open(self, parser, params, open_pos, node_index):
-        parser.begin_no_breaks()
-        return u""
+    #def open(self, parser, params, open_pos, node_index):
+    #    parser.begin_no_breaks()
+    #    return u""
 
     def render_open(self, parser, node_index):
         contents = self.get_contents(parser).strip()
@@ -88,7 +88,7 @@ class HTMLTag(postmarkup.TagBase):
 class SummaryTag(postmarkup.TagBase):
 
     def __init__(self, name, **kwargs):
-        postmarkup.TagBase.__init__(self, name, strip_first_newline=True)
+        postmarkup.TagBase.__init__(self, name, inline=True)
 
     def render_close(self, parser, node_index):
         contents = self.get_contents(parser).strip()
