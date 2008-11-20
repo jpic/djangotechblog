36eea5385a590c62173ef2c05acebc2d676d3f46
Will McGugan <x@example.com>
2008-11-20 16:54:28 +0000

    Pages model

--- techblog/apps/pages/models.py
+++ techblog/apps/pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from techblog.apps.markup import MarkupField
+
+
+class Page(models.Model):
+
+    created_time = models.DateTimeField(auto_now_add=True)
+    edit_time = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)
+
+    title = models.CharField("Page Title", max_length=100)
+    content = MarkupField("Page content")
