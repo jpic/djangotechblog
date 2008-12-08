d0d424dd3b90827eb7bfa5297b09d87339379bac
Will McGugan <x@example.com>
2008-12-08 09:47:17 +0000

    Improvements to broadcast.py

--- techblog/broadcast.py
+++ techblog/broadcast.py
@@ -12,8 +12,7 @@ class _Reciever(object):
         self.priority = priority
 
     def __call__(self, *args, **kwargs):
-        if self.callable is not None:
-            return self.callable(*args, **kwargs)
+        return self.callable(*args, **kwargs)
 
     def __cmp__(self, other):
         return cmp(self.priority, other.priority)
@@ -23,7 +22,16 @@ class Broadcaster(object):
 
     def __init__(self):
         self._lock = RLock()
-        self.recievers = {}
+        self.recievers_map = {}
+
+
+    def _get_recievers(self, func_name):
+        self._lock.acquire()
+        try:
+            return self.recievers_map.get(func_name, [])[:]
+        finally:
+            self._lock.release()
+
 
     def register(self, func_name, callable, priority=100):
 
@@ -31,42 +39,37 @@ class Broadcaster(object):
         try:
             reciever = _Reciever(func_name, callable, priority)
 
-            recievers = self.recievers.setdefault(func_name, [])
+            recievers = self.recievers_map.setdefault(func_name, [])
             recievers.append(reciever)
             recievers.sort(reverse=True)
         finally:
             self._lock.release()
 
+
     def call(self, func_name, *args, **kwargs):
         """Return the result of the highest priority callable that doesn't reject the broadcast."""
-        self._lock.acquire()
-        recievers = self.recievers.get(func_name, [])[:]
-        try:
-            for reciever in recievers:
-                try:
-                    return reciever(*args, **kwargs)
-                except RejectBroadcast:
-                    continue
-            return None
-        finally:
-            self._lock.release
+
+        recievers = self._get_recievers(func_name)
+        for reciever in recievers:
+            try:
+                return reciever(*args, **kwargs)
+            except RejectBroadcast:
+                continue
+        return None
+
 
     def first(self, func_name, *args, **kwargs):
         """Return the first result that doesn't avaluate to False."""
 
-        self._lock.acquire()
-        recievers = self.recievers.get(func_name, [])[:]
-        try:
-            for reciever in recievers:
-                try:
-                    result = reciever(*args, **kwargs)
-                    if result:
-                        return result
-                except RejectBroadcast:
-                    continue
-            return None
-        finally:
-            self._lock.release
+        recievers = self._get_recievers(func_name)
+        for reciever in recievers:
+            try:
+                result = reciever(*args, **kwargs)
+                if result:
+                    return result
+            except RejectBroadcast:
+                continue
+        return None
 
 
     def call_all(self, func_name, *args, **kwargs):
@@ -74,23 +77,23 @@ class Broadcaster(object):
         """Calls all the callables that don't return the broadcast, and
         returns a list of the return values."""
 
-        self._lock.acquire()
-        try:
-            recievers = self.recievers.get(func_name, [])[:]
-            results = []
-            for reciever in recievers:
-                try:
-                    result = reciever(*args, **kwargs)
-                except RejectBroadcast:
-                    pass
-                else:
-                    results.append(result)
-            return results
-        finally:
-            return results
+        recievers = self._get_recievers(func_name)
+        results = []
+        for reciever in recievers:
+            try:
+                result = reciever(*args, **kwargs)
+            except RejectBroadcast:
+                continue
+            results.append(result)
+        return results
+
 
     def __contains__(self, func_name):
-        return func_name in self.recievers
+        self._lock.acquire()
+        try:
+            return func_name in self.recievers_map
+        finally:
+            self._lock.release()
 
 
 class CallProxy(object):
