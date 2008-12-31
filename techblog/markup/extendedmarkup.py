6e069eefed633ba582da934883361dd688e4dcc1
Will McGugan <x@example.com>
2008-12-31 22:17:05 +0000

    Implemented serialization of extended markup to xml

--- techblog/markup/extendedmarkup.py
+++ techblog/markup/extendedmarkup.py
@@ -15,6 +15,8 @@ from django.template.context import Context
 import re
 from collections import defaultdict
 
+import xml.etree.ElementTree as ET
+
 postmarkup_renderer = postmarkup.create()
 
 class ExtendedMarkupError(Exception):
@@ -29,7 +31,8 @@ class Chunk(object):
     def __init__(self, text, chunk_type='text'):
         self.text = text
         self.chunk_type = chunk_type
-        self.vars = defaultdict(lambda: None)
+        #self.vars = defaultdict(lambda: None)
+        self.vars = {}
 
     def __str__(self):
         return "%s: %s, %s" % (self.chunk_type, repr(self.text), repr(self.vars))
@@ -54,7 +57,7 @@ class Chunk(object):
     __repr__ = __str__
 
 class SectionsDictionary(dict):
-    
+
     def __init__(self, *args, **kwargs):
         self.vars = {}
         dict.__init__(self, *args, **kwargs)
@@ -99,7 +102,7 @@ class EMarkupParser(object):
         current_lines = self.current_lines
 
         new_chunk = True
-        
+
         vars = self.sections.vars
 
         self._section_vars = {}
@@ -203,16 +206,16 @@ def parse(markup, sections=None):
 
     parser = EMarkupParser()
     parser.parse(markup)
-    sections = parser.sections    
+    sections = parser.sections
 
     rendered_sections = SectionsDictionary()
 
     error_template = select_template(['markupchunks/error.html'])
 
-    for section, chunks in sections.iteritems():                
+    for section, chunks in sections.iteritems():
 
-        content_chunks = Section(chunks.name)        
-        content_chunks.vars.update(chunks.vars)        
+        content_chunks = Section(chunks.name)
+        content_chunks.vars.update(chunks.vars)
 
         for chunk in chunks:
 
@@ -224,16 +227,16 @@ def parse(markup, sections=None):
                 chunk_html = chunk_template.render(Context(chunk_data))
             except Exception, e:
                 error = str(e)
-                chunk_html = error_template.render(Context(dict(error=error)))
+                chunk_html = unicode(error_template.render(Context(dict(error=error))))
 
             new_chunk = Chunk(chunk_html, chunk.chunk_type)
             new_chunk.vars.update(chunk.vars)
             content_chunks.append(new_chunk)
-            
+
 
         rendered_sections[section] = content_chunks
         rendered_sections.vars.update( sections[section].vars )
-        
+
 
     return rendered_sections
 
@@ -242,7 +245,7 @@ def chunks_to_html(chunks):
 
 def combine_sections(*sections_list):
 
-    combined = {}    
+    combined = {}
     for sections in filter(None, sections_list):
 
         for name_section in sections.iteritems():
@@ -254,13 +257,41 @@ def combine_sections(*sections_list):
             if section_name not in combined:
                 new_section = Section(section_name)
                 combined[section_name] = new_section
-                                    
-            if 'replace' in section.vars:
+
+            if hasattr(section, 'vars') and 'replace' in section.vars:
                 combined[section_name] = Section(section_name)
 
             combined[section_name] += section
 
     return combined
+
+
+def serialize_sections_to_xml(sections):
+
+    root = ET.Element("extendedmarkup")
+
+    sections_el = ET.SubElement(root, 'sections')
+
+    for key, value in sections.vars.iteritems():
+        var_el = ET.SubElement(root, 'var', name=key, value=value)
+
+    for name, section in sections.iteritems():
+
+        section_el = ET.SubElement(sections_el, 'section', name=name)
+
+        for key, value in section.vars.iteritems():
+            var_el = ET.SubElement(section_el, 'var', name=key, value=value)
+
+        for chunk in section:
+            chunk_el = ET.SubElement(section_el, 'chunk', type=chunk.chunk_type)
+            chunk_el.text = chunk.text
+
+            for key, value in chunk.vars.iteritems():
+                var_el = ET.SubElement(chunk_el, 'var', name=key, value=value or u"")
+
+
+
+    return ET.tostring(root)
 
 if __name__ == "__main__":
     test = """{hello=world}
@@ -299,8 +330,16 @@ This is also in a pullquote
 
 
     test1 = """
+{doc_leval_var!}
 {.body}
+{.test=1}
 Hello, World
+{..quote}
+{..chunkvar!}
+A second chunk type
+
+{.footer}
+This is the footer
 """
 
     test2 = """
@@ -310,13 +349,18 @@ Second body text
 """
 
     sections1 = parse(test1)
-    sections2 = parse(test2)    
-    
-    print chunks_to_html(combine_sections(sections1, sections2)['body'])
-    
-    
+    sections2 = parse(test2)
+
+    xml = serialize_sections_to_xml(sections1)
+    print xml
+    open("sections.xml", "w").write(xml)
+
+
+    #print chunks_to_html(combine_sections(sections1, sections2)['body'])
+
+
     #print combined['body']
-    
+
     print
     print
 
