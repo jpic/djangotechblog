794e110a063b939aa64664681b5d4da4ba6504b2
Will McGugan <x@example.com>
2009-02-16 20:19:21 +0000

    untweak

--- techblog/markup/extendedmarkup.py
+++ techblog/markup/extendedmarkup.py
@@ -45,7 +45,7 @@ class Chunk(object):
                 return int(s)
             except:
                 return s
-        return -cmp( self.get_priority(), other.get_priority() )
+        return cmp( self.get_priority(), other.get_priority() )
 
     def get_priority(self):
         try:
@@ -272,7 +272,7 @@ def process(sections, context_data):
     return sections
 
 def chunks_to_html(chunks):
-    return "\n".join( chunk.text for chunk in sorted(chunks, key=lambda chunk:chunk.vars.get('priority', 100)) )
+    return "\n".join( chunk.text for chunk in sorted(chunks, key=lambda chunk:chunk.vars.get('priority', 100), reverse=True) )
 
 def combine_sections(*sections_list):
 
