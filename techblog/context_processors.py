3166b898fc4574899b6eddc8f0a5542e16f7a4f8
Will McGugan <x@example.com>
2009-02-25 19:38:51 +0000

    New way of including GA code

--- techblog/context_processors.py
+++ techblog/context_processors.py
@@ -3,4 +3,16 @@
 from django.conf import settings
 
 def google_analytics(request):
-    return dict( GA_PATH = settings.GA_PATH )
+
+    if not settings.GA_PATH:
+        return {}
+
+    f = None
+    try:
+        f = file(settings.GA_PATH)
+        GA_CODE = f.read()
+    finally:
+        if f is not None:
+            f.close()
+
+    return dict(GA_CODE=GA_CODE)
