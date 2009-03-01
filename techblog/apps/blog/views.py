4234e7d27a00e554db1d60b886f0fd52a612522c
Will McGugan <x@example.com>
2009-03-01 22:25:09 +0000

    Potential fix for cache deleting

--- techblog/apps/blog/views.py
+++ techblog/apps/blog/views.py
@@ -38,6 +38,7 @@ def invalidate_cache(object):
         cache_key = "%s.%s" % (key_prefix, url_key)
 
         cache.delete(cache_key)
+        cache.delete(cache_key.lstrip('/'))
 
     invalidate_cache(object.get_absolute_url())
     invalidate_cache('/'+object.get_blog_relative_url())
