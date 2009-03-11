dd1d4ed0ade65699d444923f770b5b22dcbfac8c
Will McGugan <x@example.com>
2009-03-11 12:13:30 +0000

    Fixed url with missing trailing slash

--- techblog/apps/blog/tools.py
+++ techblog/apps/blog/tools.py
@@ -39,8 +39,7 @@ def collate_archives(blog, blog_root):
         return (display_time.year, display_time.month)
 
     def month_details(year, month, post_group):
-        url = "%s%i/%i" % (blog_root, year, month)
-        #url = reverse("blog_month", kwargs=dict(blog_slug=blog.slug, year=year, month=month))
+        url = "%s%i/%i/" % (blog_root, year, month)
         return url, year, month, count_iterable(post_group)
 
     months = [month_details(year, month, post_group) for (year, month),post_group in groupby(posts, year_month)]
