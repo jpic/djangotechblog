4949114ca3b3e2a3c27524be6f7aa7f1d2a3628e
Will McGugan <x@example.com>
2009-04-29 18:13:43 +0000

    Hopefully a fix for the caching issue

--- techblog/tools.py
+++ techblog/tools.py
@@ -2,9 +2,15 @@ from django.core.cache import cache
 import urlparse
 from django.conf import settings
 
+from django.utils.cache import md5_constructor, iri_to_uri
+
+def generate_cache_key(key_prefix, path):
+    """Returns a cache key for the header cache."""
+    path = md5_constructor(iri_to_uri(path))
+    return 'views.decorators.cache.cache_header.%s.%s' % (key_prefix, path.hexdigest())
+
 def clear_cached_page(path):
 
-    url_key = urlparse.urlsplit(path)[2]
     key_prefix = getattr(settings, 'CACHE_MIDDLEWARE_KEY_PREFIX', '')
-    cache_key = "views.decorators.cache.cache_header.%s.%s" % (key_prefix, url_key)
+    cache_key = generate_cache_key(key_prefix, path)
     cache.delete(cache_key)
